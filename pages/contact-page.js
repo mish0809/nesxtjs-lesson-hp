f7d4c4e4094bb1bd56064667921892a19d82709d
mish0809 <bluesky3854@gmail.com>
2021-05-30 17:53:55 +0900

    change

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout";
 import Image from "next/image";
 
-export default function Contact= ()  {
+export default function Contact ()  {
   return <Layout title="Contact">
     <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
       <div className="mt-4">
